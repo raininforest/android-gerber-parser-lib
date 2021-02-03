bd9e9e95f4734ff2e2a7ffa3b0d55791515c7dbf
Sergey <s.velesko@yandex.ru>
2021-02-03 21:38:24 +0300

    remove unnecessary constant

--- gerberparserlib/src/main/java/com/github/raininforest/gerberparserlib/enums/MacroItemType.java
+++ gerberparserlib/src/main/java/com/github/raininforest/gerberparserlib/enums/MacroItemType.java
@@ -2,6 +2,5 @@ package com.github.raininforest.gerberparserlib.enums;
 
 public enum MacroItemType {
     VARIABLE_DEFINITION,
-    MACRO_PRIMITIVE_DEFINITION,
-    NO_MACRO_ITEM_TYPE
+    MACRO_PRIMITIVE_DEFINITION
 }
