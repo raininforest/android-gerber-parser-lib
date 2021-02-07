d024f9f40747d18cd16e77c5244ba34f4b2c6021
Sergey <s.velesko@yandex.ru>
2021-02-07 19:33:04 +0300

    test covering with all valid and invalid gerber files

--- gerberparserlib/src/test/java/com/github/raininforest/gerberparserlib/SyntaxParserTest.java
+++ gerberparserlib/src/test/java/com/github/raininforest/gerberparserlib/SyntaxParserTest.java
@@ -2,29 +2,33 @@ package com.github.raininforest.gerberparserlib;
 
 import com.github.raininforest.gerberparserlib.syntaxparser.GerberFileReader;
 import com.github.raininforest.gerberparserlib.syntaxparser.SyntaxParser;
-import com.github.raininforest.gerberparserlib.syntaxparser.commands.GerberCommand;
 
-import org.junit.Before;
+import org.apache.logging.log4j.core.util.Assert;
 import org.junit.Test;
 
+import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.List;
 
 // add VM option: -Dlog4j.configurationFile="src/log4j2_config.xml"
 // to watch all log levels
 public class SyntaxParserTest {
-    private SyntaxParser syntaxParser;
 
-    @Before
-    public void init() throws FileNotFoundException {
-        GerberFileReader gerberFileReader = new GerberFileReader(
-                "src/test/test_gerbers/gerber4");
-        List<String> stringList = gerberFileReader.getStringList();
-        syntaxParser = new SyntaxParser(stringList);
+    @Test
+    public void valid_gerber_file_test() throws FileNotFoundException {
+        File testGerberDir = new File("src/test/test_gerbers");
+        File[] files = testGerberDir.listFiles();
+        for (File file: files) {
+            GerberFileReader gerberFileReader = new GerberFileReader(file.getAbsolutePath());
+            SyntaxParser syntaxParser = new SyntaxParser(gerberFileReader.getStringList());
+            syntaxParser.parse();
+        }
     }
 
     @Test
-    public void parse() {
-        final List<GerberCommand> parse = syntaxParser.parse();
+    public void invalid_gerber_file_test() throws FileNotFoundException {
+        GerberFileReader gerberFileReader = new GerberFileReader(
+                "src/test/test_gerbers/notValidFile");
+        SyntaxParser syntaxParser = new SyntaxParser(gerberFileReader.getStringList());
+        Assert.isEmpty(syntaxParser.parse());
     }
 }
